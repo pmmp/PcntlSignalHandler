475db56a8ce17c55bb42e5452455a65d9f31e106
Dylan K. Taylor <odigiman@gmail.com>
2019-01-16 15:38:09 +0000

    Initial commit

---
+++ src/dktapps/PcntlSignalHandler/Main.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace dktapps\PcntlSignalHandler;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\ClosureTask;
+use function pcntl_signal;
+use function pcntl_signal_dispatch;
+use const SIG_DFL;
+use const SIGHUP;
+use const SIGINT;
+use const SIGTERM;
+
+class Main extends PluginBase{
+
+	private const HANDLED_SIGNALS = [
+		SIGTERM,
+		SIGINT,
+		SIGHUP
+	];
+
+	public function onEnable() : void{
+		foreach(self::HANDLED_SIGNALS as $sigtype){
+			pcntl_signal($sigtype, function(int $signo, $siginfo) : void{
+				$this->getLogger()->info("Stopping the server");
+				$this->getServer()->shutdown();
+			});
+		}
+		$this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(int $unused) : void{
+			pcntl_signal_dispatch();
+		}), 5);
+	}
+
+	public function onDisable() : void{
+		foreach(self::HANDLED_SIGNALS as $sigtype){
+			pcntl_signal($sigtype, SIG_DFL);
+		}
+	}
+}
